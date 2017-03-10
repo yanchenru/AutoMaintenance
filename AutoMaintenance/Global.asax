0a1abcf9769bb2fbd2c16db7919bec2fb81ece85
Chenru Yan <yanchenru@gmail.com>
2017-03-10 08:53:38 -0700

    Add project files.

---
+++ AutoMaintenance/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AutoMaintenance.MvcApplication" Language="C#" %>
---
+++ AutoMaintenance/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AutoMaintenance.Startup))]
+namespace AutoMaintenance
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
