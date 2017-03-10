588e7b89d4674fa466215d35ed9b3385e47eccc1
Chenru Yan <yanchenru@gmail.com>
2017-03-10 11:27:42 -0700

    Vehicle controller and view

---
+++ AutoMaintenance/Controllers/VehicleController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutoMaintenance.Controllers
+{
+    public class VehicleController : Controller
+    {
+        //GET: Vehicle
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //public string Index()
+        //{
+        //    return "This is my <b>default</b> action...";
+        //}
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
